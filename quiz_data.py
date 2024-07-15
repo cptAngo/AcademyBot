73294178ce96a511658804983f42b69360168ba5
cptAngo <leha.andreevich@yandex.ru>
2024-07-15 18:20:39 +0300

    Added more questions

--- quiz_data.py
+++ quiz_data.py
@@ -8,5 +8,46 @@ quiz_data = [
         "question": "Какой тип данных используется для хранения целых чисел?",
         "options": ["int", "float", "str", "natural"],
         "correct_option": 0
+    },
+    {
+        "question": "Что выведет следующий код?\nprint(type([]))",
+        "options": ["<class 'tuple'>", "<class 'list'>", "<class 'dict'>", "<class 'set'>"],
+        "correct_option": 1
+    },
+    {
+        "question": "Какой метод используется для добавления элемента в конец списка?",
+        "options": ["add()", "insert()", "append()", "extend()"],
+        "correct_option": 2
+    },
+    {
+        "question": "Что выведет следующий код?\nx = [1, 2, 3]\ny = x\ny.append(4)\nprint(x)",
+        "options": ["[1, 2, 3]", "[1, 2, 3, 4]", "[4, 1, 2, 3]", "Ошибка"],
+        "correct_option": 1
+    },
+    {
+        "question": "Какой оператор используется для проверки равенства двух значений?",
+        "options": ["=", "!=", "<>", "=="],
+        "correct_option": 3
+    },
+    {
+        "question": "Что делает функция `len()` в Python?",
+        "options": ["Возвращает количество элементов в объекте", "Возвращает длину строки в байтах", "Возвращает тип объекта", "Возвращает максимальное значение в списке"],
+        "correct_option": 0
+    },
+    {
+        "question": "Какой из следующих вариантов является правильным способом создания словаря?",
+        "options": ["my_dict = []", "my_dict = {}", "my_dict = ()", "my_dict = set()"],
+        "correct_option": 1
+    },
+    {
+        "question": "Что выведет следующий код?\ndef func(a, b=2, c=3):\n\treturn a + b + c\n\nprint(func(1, c=4))",
+        "options": ["6", "7", "8", "Ошибка"],
+        "correct_option": 1
+    },
+    {
+        "question": "Какой из следующих вариантов является правильным способом импорта модуля `math`?",
+        "options": ["import Math", "import mathlib", "import math", "import mathematics"],
+        "correct_option": 2
     }
+    
 ]
